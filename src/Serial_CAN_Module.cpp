8da8d87ec7518ee519a6a9cf55daa221b9d12ccc
ekrimsk <ekrimsky@stanford.edu>
2022-06-20 14:20:26 -0700

    fix missing reutnr

--- src/Serial_CAN_Module.cpp
+++ src/Serial_CAN_Module.cpp
@@ -165,8 +165,12 @@ unsigned char Serial_CAN::recv(unsigned long *id, uchar *buf)
             buf[i] = dta[i+4];
         }
         return 1;
-    } else if (nbytes > 0) {
-        printf("Only %d byes AVAILABLE\n", nbytes);
+    } else if (nbytes > 12) {
+        printf("%d bytes AVAILABLE\n", nbytes);
+        return 1;
+     else if (nbytes > 0) {
+        printf("ONLY %d bytes AVAILABLE\n", nbytes);    
+        return 0;
     } else {
         printf("NO SERIAL AVAILABLE\n");
         return 0;
